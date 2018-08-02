858ada3eef29784d30ad4c97a6efbfe9dd54445b
Seamile <lanhuermao@gmail.com>
2018-08-02 16:50:51 +0800

    添加了注释

--- post/models.py
+++ post/models.py
@@ -25,10 +25,10 @@ class Post(models.Model):
 
     def update_tags(self, tag_names):
         '''更新当前 Post 的 tag'''
-        Tag.ensure_exist(tag_names)
+        Tag.ensure_exist(tag_names)  # 确保传入的 name 是存在的
 
-        update_names = set(tag_names)
-        exist_names ={t.name for t in  self.tags()}
+        update_names = set(tag_names)                # 需要全新建立关联的 name
+        exist_names ={t.name for t in  self.tags()}  # 已存在关联的 name
 
         # 筛选待创建的关系
         need_create_names = update_names - exist_names
@@ -63,10 +63,11 @@ class Tag(models.Model):
 
     @classmethod
     def ensure_exist(cls, names):
-        exist_names = {t.name for t in cls.objects.filter(name__in=names)}
-        new_names = set(names) - exist_names
-        new_tags = [cls(name=name) for name in new_names]
-        cls.objects.bulk_create(new_tags)
+        '''确保tag已存在'''
+        exist_names = {t.name for t in cls.objects.filter(name__in=names)}  # 取出已存在的 tag name
+        new_names = set(names) - exist_names               # 筛选出未创建过的 name
+        new_tags = [cls(name=name) for name in new_names]  # 产生出待创建的 tag 对象
+        cls.objects.bulk_create(new_tags)                  # 批量创建
 
     def posts(self):
         relations = PostTagRelation.objects.filter(tag_id=self.id).only('post_id')
@@ -91,11 +92,14 @@ class PostTagRelation(models.Model):
 
     @classmethod
     def add_post_tags(cls, post_id, tag_names):
-        tags = Tag.objects.filter(name__in=tag_names).only('id')
-        new_relations = [cls(post_id=post_id, tag_id=t.id) for t in tags]
-        cls.objects.bulk_create(new_relations)
+        '''批量添加 post 与 tag 的关系'''
+        tags = Tag.objects.filter(name__in=tag_names).only('id')           # 取出传入的 tags
+        new_relations = [cls(post_id=post_id, tag_id=t.id) for t in tags]  # 生成待创建的关系对象
+        cls.objects.bulk_create(new_relations)                             # 批量创建
 
     @classmethod
     def del_post_tags(cls, post_id, tag_names):
-        tag_id_list = [t.id for t in Tag.objects.filter(name__in=tag_names).only('id')]
-        cls.objects.filter(post_id=post_id, tag_id__in=tag_id_list).delete()
+        '''批量删除 post 与 tag 的关系'''
+        tags = Tag.objects.filter(name__in=tag_names).only('id')  # 取出传入的 tags
+        tag_id_list = [t.id for t in tags]                        # 生成待删除的 tag id 列表
+        cls.objects.filter(post_id=post_id, tag_id__in=tag_id_list).delete()  # 筛选、删除
