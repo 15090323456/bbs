e50c2fa433022994c1d62d966344c4397320dedd
Seamile <lanhuermao@gmail.com>
2018-08-02 18:09:28 +0800

    添加了基于角色的权限模型(未完成)

--- user/models.py
+++ user/models.py
@@ -13,7 +13,6 @@ class User(models.Model):
     plt_icon = models.CharField(max_length=256, default='')
     age = models.IntegerField(default=18)
     sex = models.CharField(max_length=8, choices=SEX)
-    perm_id = models.IntegerField()
 
     @property
     def avatar(self):
@@ -22,17 +21,52 @@ class User(models.Model):
         else:
             return self.plt_icon
 
-    @property
-    def perm(self):
-        if not hasattr(self, '_perm'):
-            self._perm = Permission.objects.get(id=self.perm_id)
-        return self._perm
-
     def has_perm(self, perm_name):
-        need_perm = Permission.objects.get(name=perm_name)
-        return self.perm.level >= need_perm.level
+        pass
 
+
+class UserRoleRelation(models.Model):
+    uid = models.IntegerField()
+    role_id = models.IntegerField()
+
+    @classmethod
+    def add_role_to_user(cls, uid, role_id):
+        pass
+
+    @classmethod
+    def del_role_from_user(cls, uid, role_id):
+        pass
+
+
+class Role(models.Model):
+    '''
+    角色表
+        admin   管理员
+        manager 版主
+        user    普通用户
+    '''
+    name = models.CharField(max_length=16, unique=True)
+
+
+class RolePermRelation(models.Model):
+    role_id = models.IntegerField()
+    perm_id = models.IntegerField()
+
+    @classmethod
+    def add_perm_to_role(cls, role_id, perm_id):
+        pass
+
+    @classmethod
+    def del_perm_from_role(cls, role_id, perm_id):
+        pass
 
 class Permission(models.Model):
-    level = models.IntegerField()
+    '''
+    权限表
+        add_post    添加帖子权限
+        del_post    删除帖子权限
+        add_comment 添加评论权限
+        del_comment 删除评论权限
+        del_user    删除用户权限
+    '''
     name = models.CharField(max_length=16, unique=True)
